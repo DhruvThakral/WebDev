5e5477f8c169999f1dffb4764dd0430c909c2ed8
Dhruv <dhruvthakral26@gmail.com>
2024-09-08 01:04:38 +0530

    ..

---
+++ Main/14_JavaScript_Part2/app.js
@@ -0,0 +1,73 @@
+// console.log("Hello from Dhruv!");
+// let a = 3;
+// let b = 4;
+// console.log("Sum: ",a+b);
+
+
+// Template Literals
+// let pencilPrice = 10;
+// let eraserPrice = 5;
+// console.log(`Total Pice: ${pencilPrice + eraserPrice} rupees`);
+
+
+// Conditional Statements
+// let name1 = "Dev";
+// if(name1 == "Dhruv"){
+//     console.log(`Welcome , ${name1}`);
+// }
+
+
+// Traffic Light
+
+// let color = "blue";
+
+// if(color == "red"){
+//     console.log("Stop!");
+// }
+// else if(color == "yellow"){
+//     console.log("Go Slow");
+// }
+// else if(color == "green"){
+//     console.log("Go!");
+// }
+// else{
+//     console.log("ERROR");
+// }
+
+
+// Pop Corn
+
+// let size = "L";
+
+// if(size == "XL"){
+//     console.log("Price : 250 Rs.");
+// }
+// else if(size == "L"){
+//     console.log("Price : 200 Rs.");
+// }
+// else if(size == "M"){
+//     console.log("Price : 100 Rs.");
+// }
+// else if(size == "S"){
+//     console.log("Price : 50 Rs.");
+// }
+// else{
+//     console.log("INCORRECT");b 
+// }
+
+
+// Nested if-else
+
+let marks = 88;
+
+if(marks >= 33){
+    console.log("Pass");
+    if(marks >= 80){
+        console.log("Grade : O");
+    }else{
+        console.log("Grade : A");
+    }
+}
+else{
+    console.log("Better Luck Next Time!");
+}
