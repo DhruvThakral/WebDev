f6e1cb6486f0bd53bfccf15e0c7ea94789e3ce0d
Dhruv <dhruvthakral26@gmail.com>
2024-09-10 18:36:17 +0530

    ..

---
+++ Main/15_JavaScript_Part3/app.js
@@ -0,0 +1,37 @@
+// let str ="      Hello      ";
+
+// console.log(str.trim());
+// console.log(str);
+
+
+// let str = "Random String";
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+// console.log(str);
+
+// let msg = "ILoveCoding";
+
+// console.log(msg.indexOf("Love"));
+
+
+
+// let msg = "     hello       ";
+// let newMsg = msg.trim();
+// console.log(newMsg);
+
+
+// let str = "ILoveCoding";
+// console.log(str.slice(1,5));
+
+
+let msg = "help!";
+console.log(msg.trim().toUpperCase());
+
+
+let name1 = "ApnaCollege";
+console.log(name1.slice(4,9));   //Colle
+console.log(name1.indexOf("na"));   // 2
+console.log(name1.replace("Apna","Our"));   //"OurCollege"
+
+let newName1 = name1.slice(4).replace("l","t");     //"Cotlege"
+console.log(newName1);
