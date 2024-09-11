3d2bfe6bd5c9a73767bd4428636c399c15db2121
Dhruv <dhruvthakral26@gmail.com>
2024-09-11 19:36:08 +0530

    ..

---
+++ Main/15_JavaScript_Part3/app2.js
@@ -0,0 +1,13 @@
+let students = ["aman","raman","sahil"];
+
+let nums = [2,4,6,8];
+
+let info = ["aman",25,6.1];
+
+let newArr = [];
+
+let fruits=["banana","apple","orange"];
+
+let months = ["january","july","march","august"];
+
+
