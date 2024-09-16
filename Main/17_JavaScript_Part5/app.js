e714a76820d8d778465b9bb4675ab05936e9b020
Dhruv <dhruvthakral26@gmail.com>
2024-09-16 14:27:02 +0530

    ..

---
+++ Main/17_JavaScript_Part5/app.js
@@ -0,0 +1,21 @@
+const Student = {
+    name : "Dhruv",
+    age : 21,
+    marks : 94.4
+};
+
+
+let item = {
+    price : 100.99,
+    color : ["red","pink","yellow"],
+    discount : "6%"
+};
+
+
+const post = {
+    username : "@dhruvthakral26",
+    content : "This is my #firstPost.",
+    likes : 150,
+    reposts : 5,
+    tags : ["@delta","@InGOV"]
+};
