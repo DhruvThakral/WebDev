b91604e6de8daa756dc1394c04b864cc91bc6173
Dhruv <dhruvthakral26@gmail.com>
2024-10-10 22:19:09 +0530

    ..

---
+++ Main/20_JavaScript_Part8/app.js
@@ -0,0 +1,75 @@
+// let arr=[1,2,3,4,5];
+
+// arr.forEach(function(el){
+//     console.log(el);
+// });
+
+// let obj = [
+//     {
+//         name : "A",
+//         marks : 94,
+//     },
+//     {
+//         name : "B",
+//         marks : 92,
+//     },
+//     {
+//         name : "C",
+//         marks : 99,
+//     }
+// ];
+
+// obj.forEach((student) => {
+//     console.log(student.name);
+// })
+
+
+
+
+// let num = [1,2,3,4,5];
+
+// let newArr = num.map(function(el){
+//     return el*2;
+// });
+
+// console.log(newArr);
+
+// let obj = [
+//     {
+//         name : "A",
+//         marks : 94,
+//     },
+//     {
+//         name : "B",
+//         marks : 92,
+//     },
+//     {
+//         name : "C",
+//         marks : 99,
+//     }
+// ];
+
+// let gpa = obj.map((student)=>{
+//     return student.marks/10;
+// });
+
+// console.log(gpa);
+
+
+// let nums = [1,2,3,4,5,6,7,8,9,10];
+
+// let newArr = nums.filter((el) => {
+//     return (el % 2 == 0);
+// });
+
+// console.log(newArr);
+
+
+
+// [1,2,3,4,5,6].every((el) => {
+//     el % 2 == 0;
+// })
+
+
+
+
