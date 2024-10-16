175b3d8ff7af5b0806a89cfa25154f4baf3537af
Dhruv <dhruvthakral26@gmail.com>
2024-10-16 23:16:41 +0530

    ..

--- Main/22_JavaScript_Part10/app.js
+++ Main/22_JavaScript_Part10/app.js
@@ -1,16 +1,46 @@
-let btns = document.querySelectorAll('button');
+// let btns = document.querySelectorAll('button');
 
-function sayHello(){
-    alert("Hello");
-}
+// function sayHello(){
+//     alert("Hello");
+// }
 
-function sayName(){
-    alert("Dhruv");
-}
+// function sayName(){
+//     alert("Dhruv");
+// }
 
-for(btn of btns){
-    btn.addEventListener("click", sayHello);
-        btn.addEventListener("click",sayName);
+// for(btn of btns){
+//     btn.addEventListener("click", sayHello);
+//         btn.addEventListener("click",sayName);
 
-    btn.addEventListener("dbclick", function() { console.log("You double clicked me !"); });
-}
+//     btn.addEventListener("dbclick", function() { console.log("You double clicked me !"); });
+// }
+
+
+// let p = document.querySelector("p");
+
+// p.addEventListener("click",function(){
+//     console.log("This is a parah.");
+// })
+
+// let box = document.querySelector(".box");
+
+// box.addEventListener("mouseenter",function(){
+//     console.log("mouse inside the box.");
+// });
+
+
+
+let btn = document.querySelector("button");
+let h1 = document.querySelector("h1");
+let h3 = document.querySelector("h3");
+let p = document.querySelector("p");
+
+function changeColor(){
+    console.log(this.innerText);
+    this.style.backgroundColor="yellow";
+};
+
+btn.addEventListener("click",changeColor);
+h1.addEventListener("click",changeColor);
+h3.addEventListener("click",changeColor);
+p.addEventListener("click",changeColor);
