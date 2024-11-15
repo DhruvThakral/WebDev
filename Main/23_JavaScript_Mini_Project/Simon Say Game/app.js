b9ca91d3be6d2deda735113888de2dd1800a6521
Dhruv <dhruvthakral26@gmail.com>
2024-11-15 15:50:23 +0530

    ..

---
+++ Main/23_JavaScript_Mini_Project/Simon Say Game/app.js
@@ -0,0 +1,86 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["red", "green", "yellow" , "purple"];
+
+let level = 0;
+let started = false;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress",function(){
+    if(started == false){
+        console.log("Game started!");
+        started = true;
+
+        levelUp();
+    }
+});
+
+function gameFlash(btn){
+    btn.classList.add("flash");
+
+    setTimeout(function(){
+        btn.classList.remove("flash")
+    },250);
+};
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+
+    setTimeout(function(){
+        btn.classList.remove("userflash")
+    },250);
+};
+
+function levelUp(){
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+
+    // console.log(randIdx);
+    // console.log(randColor);
+    // console.log(randBtn);
+
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+
+    gameFlash(randBtn);
+}
+
+function checkAns(){
+    let idx = level-1;
+    if(userSeq[idx] == gameSeq[idx]){
+        console.log("same value");
+    }else{
+        h2.innerText = "Game Over ! Press any key to restart the game.";
+    }
+}
+
+function btnPress() {
+    let btn = this;
+    console.log(this);
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns();
+}
+
+
+let allBtns = document.querySelectorAll(".btn");
+
+for(btn of allBtns){
+    btn.addEventListener("click", btnPress);
+}
+
+
+
+
+
+
+
--- Main/23_JavaScript_Mini_Project/To Do app/app.js
+++ Main/23_JavaScript_Mini_Project/To Do app/app.js
@@ -1,5 +1,37 @@
-let button = document.querySelector("button");
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
 
-button.addEventListener("click", function(){
-    
-})
+btn.addEventListener("click", function(){
+    // Creating li and adding it to ul
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+    ul.appendChild(item);
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.classList.add("delete");
+    item.appendChild(delBtn);
+
+    inp.value = "";              // Resetting to empty string
+});
+
+
+// let delBtns = document.querySelectorAll(".delete");
+// for(delBtn of delBtns){
+//     delBtn.addEventListener("click", function(){
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     });
+// }
+
+
+// Event Delegation
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName == "BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log("Deleted!");
+    }
+});
