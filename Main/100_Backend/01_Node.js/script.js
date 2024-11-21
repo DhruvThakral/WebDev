90cbf6eb7feaf407e884a6f264ae6f003971e416
Dhruv <dhruvthakral26@gmail.com>
2024-11-21 22:56:13 +0530

    ..

---
+++ Main/100_Backend/01_Node.js/script.js
@@ -0,0 +1,22 @@
+// let n = 5;
+
+// for(let i=0; i<n; i++){
+//     console.log("Hello Dhruv",i);
+// }
+
+// console.log("Bye!");
+
+
+
+// let args = process.argv;
+
+// for (let i=2; i<args.length; i++){
+//     console.log("Hello to ",args[i]);
+// }
+
+// console.log("Bye!");
+
+
+const someValue = require("./math");
+
+console.log(someValue);
